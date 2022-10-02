8b63e9a6bd678da0c1002c96dbc7eb0e99b5f41b
miris-mimitako <miris.mikitako@gmail.com>
2022-10-02 23:19:41 +0900

    add: csv analyzer

--- analyzer/statbasic/admin.py
+++ analyzer/statbasic/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import User
+from .models import User, CsvAnalyzer
 
 admin.site.register(User)
--- analyzer/statbasic/models.py
+++ analyzer/statbasic/models.py
@@ -1,3 +1,4 @@
+from enum import unique
 from django.db import models
 
 # Create your models here.
@@ -23,4 +24,23 @@ class User(models.Model):
     def save(self, *args, **kwargs):
         if not self.user_slug:
             self.user_slug = slugify(self.user_name)
+        return super().save(*args, **kwargs)
+
+class CsvAnalyzer(models.Model):
+    csv_file_name  = models.CharField(max_length=250, default="add_new_csv", unique=True)
+    csv_comment = models.TextField(blank = True, null = True)
+    csv_file_path = models.TextField(blank = True, null = True)
+    csv_data_type = models.JSONField(blank = True, null = True)
+    csv_analyze_selection = models.JSONField(blank = True, null = True)
+    csv_analyze_record = models.JSONField(blank = True, null = True)
+    csv_slug = models.SlugField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    revised_at = models.DateTimeField(auto_now_add = True)
+
+    def get_absolute_url(self):
+        return reverse("csv_detail", kwargs={"slug": self.csv_slug})
+
+    def save(self, *args, **kwargs):
+        if not self.csv_slug:
+            self.csv_slug = slugify(self.csv_file_name)
         return super().save(*args, **kwargs)
